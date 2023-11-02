ad5518822baea1dd5a6c7f68eb2b0e0e36fee8b1
trishaDas13 <trisha.das1308@gmail.com>
2023-11-02 16:25:48 +0530

    added style and small details

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ async function fetchAPI(){
     }
     if(input.value == ""){
         movieContainer.innerHTML = "Please Enter a search term";
+        paginationStore.style.opacity = '0';
     }
     createObject(res);
     
@@ -36,7 +37,7 @@ function debounce(fetchAPI, delay){
         }, delay);
     }
 }
-let searchResults = debounce(fetchAPI, 500);
+let searchResults = debounce(fetchAPI, 400);
 
 //todo: -------- Create Object --------
 function createObject(res){
@@ -47,7 +48,8 @@ function createObject(res){
             poster : item.Poster,
         }
     });
-    appendMovie(newMovieArray);   
+    appendMovie(newMovieArray);  
+    lastPage(Math.ceil(res.totalResults/10));
 }
 //todo: -------- Append Movies ---------
 function appendMovie(newMovieArray){
@@ -58,7 +60,7 @@ function appendMovie(newMovieArray){
         movie.classList.add('movie');
         
         movie.innerHTML = `
-            <img src="${item.poster}" alt="error" onclick = "showDialog('${item.poster}')">
+            <img src="${item.poster}" alt="error" onclick = "showDialog('${item.poster}', '${item.name}')">
             <p class="title">Title : <span>${item.name}</span></p>
             <p class="year">Released Year : <span>${item.year}</span></p>
         `
@@ -66,42 +68,96 @@ function appendMovie(newMovieArray){
     });
 }
 //todo: -------- Show Dialog Box ---------
-function showDialog(link){
-    let imageLink = link;
+function showDialog(link, name){
     mydialog.show();
     modal[0].innerHTML = `
-        <div class="modalImg">
-            <img src='${imageLink}' alt="">
-            <button class="close" onclick="closeDialog()"> Close </button>
-        </div>
+    <div class="modalImg">
+    <div class="image">
+        <img src='${link}' alt="Error">
+        <p class = "title">${name}</p> 
+    </div>
+    <div class="text">
+        <p class="review">Review: <span> </span> </p>
+        <textarea class="textarea" cols="30" rows="10">Write your review here... </textarea>
+        <button> Save </button>
+    </div>
+    <button class="close" onclick="closeDialog()"> ❌ </button>
+</div>
     `;
+
+    let saveBtn = modal[0].querySelector('button');
+    let text = modal[0].querySelector('textarea');
+    let reviewContent = modal[0].querySelector('.review span');
+
+    text.addEventListener('keyup', (e) =>{
+        saveText(e.target.value, name);
+    });
+    saveBtn.addEventListener('click', () => {
+        updateReview(text, reviewContent, name);
+    });
+    if(localStorage.getItem(`reviews/${name}`)){
+        reviewContent.innerHTML = localStorage.getItem(`reviews/${name}`);
+        text.style.display = 'none';
+    }
 }
-//todo: --------- Close Dialog Box---------
+//todo: --------- Close Dialog Box ---------
 function closeDialog() {
     mydialog.close();
 }
+//todo: --------- Save in local storage ---------
+function saveText(text, name){
+    localStorage.setItem(`reviews/${name}`, text);
+}
+//todo: --------- update review ---------
+function updateReview(text, reviewContent){
+    reviewContent.innerHTML = text.value;
+    text.style.display = 'none';
+}
 //todo: -------- Pagination ---------
 function pagination(){
-    let totalPages = Math.ceil(total / 10);
     let pageStore = document.createElement('div');
     pageStore.classList.add('pageStore')
     pageStore.innerHTML = `
-        <button ${pageCount === 1 ? 'disabled' : ''} onclick="navigate(${pageCount-1})">Prev.</button>
+        <button onclick="navigatePrev()">
+            <svg width="35" height="35" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill="#000000" d="m4 10l9 9l1.4-1.5L7 10l7.4-7.5L13 1z"/>
+            </svg>
+        </button>
         <p class="paginationPara">
-            Page <span>${pageCount}</span> of <span>${totalPages}</span>
+            Page <span>${pageCount}</span> of <span class="totalCount"></span>
         </p>
-        <button ${pageCount === totalPages ? 'disabled' : ''} onclick="navigate(${pageCount+1})">Next</button>
+        <button onclick="navigateNext()">
+            <svg width="35" height="35" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill="#000000" d="M7 1L5.6 2.5L13 10l-7.4 7.5L7 19l9-9z"/>
+            </svg>
+        </button>
     `
     paginationStore.appendChild(pageStore);
 }
 pagination();
-//todo: -------- Pagination ---------
-function navigate(page){
-    if((page >= pageCount ) && page <= Math.ceil(total / 10)){
-        pageCount = page;
+function handler(){
+    document.querySelector('.paginationPara span').textContent= pageCount;
+}
+function lastPage(lastPageIndex){
+    document.querySelector('.paginationPara .totalCount').textContent= lastPageIndex;
+}
+//todo: -------- activate prev button ---------
+function navigatePrev(){
+    if(pageCount > 1){
+        pageCount--;
+        searchResults();
+        handler();
     }
 }
-
-
-input.addEventListener('input', searchResults);
+//todo: -------- activate next button ---------
+function navigateNext(){
+    pageCount++;
+    searchResults();
+    handler();
+}
+input.addEventListener('input', () =>{
+    pageCount = 1;
+    searchResults();
+    paginationStore.style.opacity = '1';
+});
 
